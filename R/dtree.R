eeb02cd4d36c5fa1a06ac330219eb7279462527a
Henrique Souza <htssouza@gmail.com>
2016-10-14 00:34:36 -0300

    Improvements

--- R/dtree.R
+++ R/dtree.R
@@ -42,9 +42,40 @@ set.seed(1994)
 # Decision Tree Implementation
 ########################################################################################################################
 
-passengerData <- LoadPassengerData()
+validationFactors <- c(.25, .28, .3, .32, .34, .4, .5)
+results <- data.table(validationFactor=validationFactors)
+results[, result := 0]
 
-# train
+for(.validationFactor in validationFactors) {
+  # load
+  passengerData <- LoadPassengerData(validationFactor = .validationFactor)
+
+  # train
+  train <- passengerData$train
+  train <- Normalize(train)
+  fit <- rpart(GetFormula(train),
+               data=train,
+               method="class")
+
+  # validation
+  validation <- passengerData$validation
+  validation <- Normalize(validation)
+  validation.result <- predict(fit, validation, type = "class")
+
+  # evaluation
+  .result <- Evaluate(validation.result, validation[, survived])
+  results[ validationFactor == .validationFactor, result := .result ]
+
+  print(paste0("validationFactor = ", .validationFactor, ", result = ", .result))
+}
+
+print(results)
+betterResult <- (results[order(-result), result])[1]
+betterValidationFactor <- (results[result == betterResult, validationFactor])[1]
+print(paste0("betterValidationFactor = ", betterValidationFactor, ", betterResult = ", betterResult))
+
+betterValidationFactor <- .25
+passengerData <- LoadPassengerData(validationFactor = betterValidationFactor)
 train <- passengerData$train
 train <- Normalize(train)
 fit <- rpart(GetFormula(train),
@@ -54,13 +85,6 @@ fit <- rpart(GetFormula(train),
 plot(fit)
 text(fit)
 fancyRpartPlot(fit)
-
-# validation
-validation <- passengerData$validation
-validation <- Normalize(validation)
-validation.result <- predict(fit, validation, type = "class")
-
-print(paste0("Result = ", Evaluate(validation.result, validation[, survived])))
 
 # test
 test <- passengerData$test
